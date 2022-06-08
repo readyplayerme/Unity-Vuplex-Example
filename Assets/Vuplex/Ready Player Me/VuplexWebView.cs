fb60a6637b5b6c50d6c072661d002dd6b3df6b91
Harrison <harrisonhough4@gmail.com>
2022-06-08 17:08:42 +0300

    feat: added RPM plugin

---
+++ Assets/Vuplex/Ready Player Me/VuplexWebView.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using UnityEngine;
+using Vuplex.WebView;
+using Newtonsoft.Json;
+
+public class VuplexWebView
+{
+    private CanvasWebViewPrefab webView;
+
+    private const string DataUrlFieldName = "url";
+    private const string AvatarExportEventName = "v1.avatar.exported";
+
+    public Action OnInitialized;
+    public Action<string> OnAvatarUrlReceived;
+
+    public void Initialize(CanvasWebViewPrefab prefab)
+    {
+        webView = prefab;
+
+        PartnerSO partner = Resources.Load<PartnerSO>("Partner");
+        string url = partner.GetUrl();
+        webView.InitialUrl = url;
+        webView.DragMode = DragMode.DragWithinPage;
+
+        webView.Initialized += (object sender, EventArgs args) =>
+        {
+            Debug.Log("--- INIT");
+
+            webView.WebView.LoadProgressChanged -= OnLoadProgressChanged;
+            webView.WebView.LoadProgressChanged += OnLoadProgressChanged;
+        };
+    }
+
+    private void OnLoadProgressChanged(object sender, ProgressChangedEventArgs args)
+    {
+        Debug.Log($"--- PROGRESS: {args.Progress} - {args.Type}");
+
+        if (args.Type == ProgressChangeType.Finished)
+        {
+            webView.WebView.MessageEmitted -= OnMessageReceived;
+            webView.WebView.MessageEmitted += OnMessageReceived;
+
+            webView.WebView.ExecuteJavaScript(@"
+                function ready() {
+                    document.cookie = 'webview=true';
+
+                    window.removeEventListener('message', subscribe)
+                    window.addEventListener('message', subscribe)
+
+                    window.postMessage(
+                        JSON.stringify({
+                            target: 'readyplayerme',
+                            type: 'subscribe',
+                            eventName: 'v1.**'
+                        }),
+                        '*'
+                    );
+                }
+
+                function subscribe(event) {
+                    // post message v1, this will be deprecated
+                    if(event.data.endsWith('.glb')) {
+                        window.vuplex.postMessage(event.data)
+                    }
+                    // post message v2
+                    else {
+                        const json = parse(event);
+                        const source = json.source;
+                    
+                        if (source !== 'readyplayerme') {
+                            return;
+                        }
+
+			            window.vuplex.postMessage(event.data)
+                    }
+		        }
+
+                function parse(event) {
+                    try {
+                        return JSON.parse(event.data);
+                    } catch (error) {
+                        return null;
+                    }
+                }
+
+                if (window.vuplex) {
+                    ready();
+                } else {
+                    window.removeEventListener('vuplexready', ready);
+                    window.addEventListener('vuplexready', ready);
+                }
+            ", OnJsExecuted);
+        }
+    }
+
+    private void OnJsExecuted(string result)
+    {
+        Debug.Log($"--- JS EXECUTED: {result}");
+
+        OnInitialized?.Invoke();
+    }
+
+    private void OnMessageReceived(object sender, EventArgs<string> args)
+    {
+        Debug.Log($"OnMessageReceived: { args.Value }");
+
+        try
+        {
+            WebMessage webMessage = JsonConvert.DeserializeObject<WebMessage>(args.Value);
+
+            if (webMessage.eventName == AvatarExportEventName)
+            {
+                if (webMessage.data.TryGetValue(DataUrlFieldName, out string avatarUrl))
+                {
+                    OnAvatarUrlReceived?.Invoke(avatarUrl);
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.Log($"OnMessageReceived: { args.Value }\nError Message: { e.Message }");
+        }
+    }
+}
---
+++ Assets/Vuplex/Ready Player Me/VuplexWebViewTest.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using ReadyPlayerMe;
+using Vuplex.WebView;
+
+public class VuplexWebViewTest : MonoBehaviour
+{
+    private GameObject avatar;
+    private AvatarLoader avatarLoader;
+    private VuplexWebView vuplexWebView;
+
+    [SerializeField] private GameObject loading;
+    [SerializeField] private CanvasWebViewPrefab canvasWebView;
+
+    private void Start()
+    {
+        vuplexWebView = new VuplexWebView();
+        vuplexWebView.Initialize(canvasWebView);
+        vuplexWebView.OnInitialized = () => Debug.Log("WebView Initialized");
+        vuplexWebView.OnAvatarUrlReceived = OnAvatarUrlReceived;
+    }
+
+    // WebView callback for retrieving avatar url
+    private void OnAvatarUrlReceived(string url)
+    {
+        loading.SetActive(true);
+        avatarLoader = new AvatarLoader();
+        avatarLoader.LoadAvatar(url, OnAvatarImported, OnAvatarLoaded);
+    }
+
+    // AvatarLoader callback when import is done
+    private void OnAvatarImported(GameObject avatar)
+    {
+        avatar.gameObject.SetActive(false);
+        avatar.transform.Rotate(Vector3.up, 180);
+        avatar.transform.position = new Vector3(0.75f, 0, 0.75f);
+        Debug.Log("Avatar Imported");
+    }
+
+    // AvatarLoader callback for retrieving loaded avatar game object
+    private void OnAvatarLoaded(GameObject avatar, AvatarMetaData metaData)
+    {
+        Destroy(this.avatar);
+
+        this.avatar = avatar;
+        avatar.gameObject.SetActive(true);
+
+        loading.SetActive(false);
+        Debug.Log("Avatar Loaded");
+    }
+}
